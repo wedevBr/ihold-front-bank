7ca0f2dd98a4f8256cc83b41e0689abc37ca506a
Allisson Lima <Allysoncosta@outlook.com>
2022-10-11 17:35:43 -0300

    feat: add component modal e ajuste modal pagamento

---
+++ src/components/Modals/ModalSuccess/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+export function ModalSuccess() {
+  return (
+    <Box h="300px">
+      <Flex bg="#27AE60" color="#fff" justify="center" align="center" h="80px">
+        <Text>ModalSuccess</Text>
+      </Flex>
+    </Box>
+  );
+}
