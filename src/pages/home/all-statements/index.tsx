7bc99484bed801a251c6654782265c2c976cef5f
Allisson Lima <Allysoncosta@outlook.com>
2022-10-25 16:46:23 -0300

    feat: add tablet new | all statemnets

--- src/pages/home/all-statements/index.tsx
+++ src/pages/home/all-statements/index.tsx
@@ -1,11 +1,19 @@
 import {
   Box,
   Button,
+  Center,
   Flex,
   SimpleGrid,
   Skeleton,
+  Table,
+  TableContainer,
   TabPanel,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
+  Tr,
   useDisclosure,
   useToast,
 } from '@chakra-ui/react';
@@ -23,6 +31,7 @@ import {
   ExtractAllTable,
   Input,
   Layout,
+  Loading,
   Modal,
   Select,
 } from '~/components';
@@ -35,6 +44,7 @@ import {
 import { formatCalcValue } from '~/utils/formatValue';
 import { routeTransactions } from '..';
 import { createPagination } from '~/hooks/createPagination';
+import { truncate } from '~/utils/truncate';
 
 const dowloadSchema = yup.object().shape({
   date_start: yup.string().required('Período inicial obrigatório'),
@@ -44,8 +54,14 @@ const dowloadSchema = yup.object().shape({
 
 export default function AllStatements() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: IsOpenFilter,
+    onOpen: onOpenFilter,
+    onClose: onCloseFilter,
+  } = useDisclosure();
 
   const [filterDate, setFilterDate] = useState('');
+  const [t, T] = useState<any>();
   const [activeFilter, setActiveFilter] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [per_page, setPerPage] = useState(25);
@@ -157,8 +173,9 @@ export default function AllStatements() {
       <Box bg="#FFFFFF" p="50px" mt="30px" borderRadius="10px">
         <Box w="full" borderRadius="6px" h="">
           <Text>Extrato: iHold Bank </Text>
-          <Flex my="10px" w="300px" justify="space-between" mb="25px">
-            {/* <Button
+          <Flex my="10px" mb="25px" justify="space-between" w="full">
+            <Flex w="300px" justify="space-between">
+              {/* <Button
             transition="all linear .55s"
             variant="unstyled"
             fontSize="14px"
@@ -172,38 +189,44 @@ export default function AllStatements() {
           >
             Todos
           </Button> */}
-            {dates.map((day, key) => (
-              <Button
-                key={key}
-                transition="all linear .55s"
-                variant="unstyled"
-                w="67px"
-                h="26"
-                fontSize="14px"
-                borderRadius="52px"
-                border="1px solid #CBD3E0"
-                color={+filterDate === day ? '#fff' : ''}
-                bg={+filterDate === day ? '#2E4EFF' : ''}
-                onClick={() => {
-                  setActiveFilter(!activeFilter);
-                  if (+day !== +filterDate) {
-                    setCurrentPage(1);
-                    setFilterDate(day.toString());
-                    return;
-                  } else if (!activeFilter) {
-                    setCurrentPage(1);
-                    setFilterDate(day.toString());
-                    return;
-                  }
-                  setFilterDate('');
-                }}
-              >
-                {day} dias
-              </Button>
-            ))}
+              {dates.map((day, key) => (
+                <Button
+                  key={key}
+                  transition="all linear .55s"
+                  variant="unstyled"
+                  w="67px"
+                  h="26"
+                  fontSize="14px"
+                  borderRadius="52px"
+                  border="1px solid #CBD3E0"
+                  color={+filterDate === day ? '#fff' : ''}
+                  bg={+filterDate === day ? '#2E4EFF' : ''}
+                  onClick={() => {
+                    setActiveFilter(!activeFilter);
+                    if (+day !== +filterDate) {
+                      setCurrentPage(1);
+                      setFilterDate(day.toString());
+                      return;
+                    } else if (!activeFilter) {
+                      setCurrentPage(1);
+                      setFilterDate(day.toString());
+                      return;
+                    }
+                    setFilterDate('');
+                  }}
+                >
+                  {day} dias
+                </Button>
+              ))}
+            </Flex>
+            <Flex align="center" cursor="pointer" onClick={onOpenFilter}>
+              <Icon icon="akar-icons:calendar" color="#21C6DE" width={20} />
+              <Text ml="5px">Filtrar por data</Text>
+            </Flex>
           </Flex>
+
           <ContainerTransaction
-            tabName={['todos']}
+            tabName={['completo']}
             header={
               <Button
                 bg="#2E4EFF"
@@ -230,8 +253,129 @@ export default function AllStatements() {
               </Button>
             }
           >
-            <TabPanel>
-              <ExtractAllTable isLoading={isFetching} items={DataPix} />
+            <TabPanel px="0">
+              {isFetching ? (
+                <Center h="500px">
+                  <Loading />
+                </Center>
+              ) : (
+                <Box overflow="hidden">
+                  <TableContainer
+                    h="500px"
+                    w="full"
+                    pos="relative"
+                    // borderRadius="10px"
+                    overflowY="auto"
+                  >
+                    <Table variant="unstyled" size="sm">
+                      <Thead w="full">
+                        <Tr
+                          // h="40px"
+                          top={0}
+                          // zIndex={2000}
+                          pos="sticky"
+                          bg="#F0F0F3"
+                        >
+                          <Th>DATA</Th>
+                          <Th>TIPO</Th>
+                          <Th>NOME</Th>
+                          <Th>VALOR</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        {DataPix &&
+                          DataPix.data?.map((item, key) => {
+                            return (
+                              <Tr key={key} borderTop="1px solid #CBD3E0">
+                                <Td maxW="20px" w="10px">
+                                  {item?.completed_at !== item?.completed_at
+                                    ? null
+                                    : moment(item?.completed_at)
+                                        .format('DD MMM')
+                                        .toUpperCase()}
+                                </Td>
+                                <Td maxW="20px" w="10px">
+                                  <Box
+                                    borderRadius="5px"
+                                    p="8px"
+                                    background={
+                                      item?.operation === 'cash-in'
+                                        ? '#27ae6033'
+                                        : '#ff313b33'
+                                    }
+                                  >
+                                    {item?.operation === 'cash-in' ? (
+                                      <Icon
+                                        icon="bi:arrow-90deg-up"
+                                        color="#27AE60"
+                                        width={16}
+                                      />
+                                    ) : (
+                                      <Icon
+                                        icon="bi:arrow-90deg-down"
+                                        color="#F03D3E"
+                                        width={16}
+                                      />
+                                    )}
+                                  </Box>
+                                </Td>
+                                <Td minW="200px" w="100px">
+                                  <Box>
+                                    <Text color="#070A0E" fontWeight={600}>
+                                      {item?.description.includes(
+                                        'TRANSFERÊNCIA ENVIADA'
+                                      )
+                                        ? 'TRANSFERÊNCIA ENVIADA'
+                                        : item?.description.includes(
+                                            'TRANSFERÊNCIA RECEBIDA'
+                                          )
+                                        ? 'TRANSFERÊNCIA RECEBIDA'
+                                        : item?.description.includes(
+                                            'PIX RECEBIDO'
+                                          )
+                                        ? 'PIX RECEBIDO'
+                                        : item?.description.includes(
+                                            'PIX ENVIADO'
+                                          )
+                                        ? 'PIX ENVIADO'
+                                        : ''}
+                                    </Text>
+                                    <Text>
+                                      {item?.operation === 'cash-out'
+                                        ? item?.metadata?.recipient?.name
+                                          ? item?.metadata?.recipient?.name
+                                          : item?.metadata?.payload?.merchant
+                                              ?.name
+                                        : item?.metadata?.sender?.name
+                                        ? item?.metadata?.sender?.name
+                                        : item?.metadata?.payload?.merchant
+                                            ?.name}
+                                    </Text>
+                                    <Text color="#7F8B9F">
+                                      {moment(item?.completed_at).format('LT')}
+                                    </Text>
+                                  </Box>
+                                </Td>
+                                <Td
+                                  color={
+                                    item?.operation === 'cash-in'
+                                      ? '#27AE60'
+                                      : '#F03D3E'
+                                  }
+                                >
+                                  {item?.operation === 'cash-in'
+                                    ? `R$${item?.amount}`
+                                    : `-  R$${item?.amount}`}
+                                </Td>
+                              </Tr>
+                            );
+                          })}
+                      </Tbody>
+                    </Table>
+                  </TableContainer>
+                </Box>
+              )}
+              {/* <ExtractAllTable isLoading={isFetching} items={DataPix} /> */}
               <Flex align="center" w="full" justify="center" mt="40px">
                 <Box cursor={!!DataPix?.links.prev ? 'pointer' : 'not-allowed'}>
                   <Icon
@@ -383,6 +527,78 @@ export default function AllStatements() {
             isLoading={loading}
           >
             DOWLOAD
+          </Button>
+        </Box>
+      </Modal>
+      <Modal
+        isOpen={IsOpenFilter}
+        onClose={onCloseFilter}
+        title="FILTRAR POR PERÍODO"
+      >
+        <Box as="form" onSubmit={handleSubmit(handleDowloadExtract)}>
+          <Flex
+            align="center"
+            w="full"
+            justify="space-between"
+            mb="20px"
+            minH="80px"
+          >
+            <Input
+              label="Período Inicial"
+              labelColor="#7F8B9F"
+              w="full"
+              size="sm"
+              bg="transparent"
+              color="#7F8B9F"
+              type="date"
+              fontSize="16px"
+              border="0px"
+              borderBottom="1px solid #7F8B9F"
+              borderRadius={0}
+              placeholder="dd/mm/aaaa"
+              _focus={{
+                borderBottom: '1px solid #2E4EFF',
+              }}
+              {...register('date_start')}
+              error={formState?.errors?.date_start}
+            />
+            <Input
+              label="Período Final"
+              labelColor="#7F8B9F"
+              w="full"
+              size="sm"
+              bg="transparent"
+              color="#7F8B9F"
+              type="date"
+              fontSize="16px"
+              border="0px"
+              borderBottom="1px solid #7F8B9F"
+              borderRadius={0}
+              placeholder="dd/mm/aaaa"
+              _focus={{
+                borderBottom: '1px solid #2E4EFF',
+              }}
+              {...register('date_end')}
+              error={formState?.errors?.date_end}
+            />
+          </Flex>
+          <Button
+            my="20px"
+            bg="#2E4EFF"
+            _hover={{ bg: '#435ffa' }}
+            _active={{ bg: '#2444f6' }}
+            color="#fff"
+            w="full"
+            fontSize="0.875rem"
+            borderRadius="20px"
+            h="35px"
+            textTransform="uppercase"
+            fontWeight="600"
+            padding="8px 1.25rem"
+            // type="submit"
+            isLoading={loading}
+          >
+            Filtrar
           </Button>
         </Box>
       </Modal>
