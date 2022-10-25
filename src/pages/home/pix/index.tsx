04f2f0d6932d99b1657d6332279ab7ceea2236da
Allisson Lima <Allysoncosta@outlook.com>
2022-10-25 11:09:52 -0300

    refactor: statements pixs and pagination useQuery

--- src/pages/home/pix/index.tsx
+++ src/pages/home/pix/index.tsx
@@ -3,6 +3,8 @@ import {
   Button,
   Flex,
   SimpleGrid,
+  Skeleton,
+  Spinner,
   TabPanel,
   Text,
   useDisclosure,
@@ -29,9 +31,11 @@ import {
   GetAllStatementsOperation,
   GetStatementsDownloadExtract,
   GetStatementsOperation,
+  useCustomers,
 } from '~/services/hooks/useStatements';
 import { formatCalcValue } from '~/utils/formatValue';
 import { routeTransactions } from '..';
+import { createPagination } from '~/hooks/createPagination';
 
 const dowloadSchema = yup.object().shape({
   date_start: yup.string().required('Período inicial obrigatório'),
@@ -40,6 +44,29 @@ const dowloadSchema = yup.object().shape({
 });
 
 export default function Pix() {
+  const [filterDate, setFilterDate] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [per_page, setPerPage] = useState(25);
+  const dates = [7, 15, 30, 60];
+  const date = new Date();
+  const formatPrevDate = (days: number) => {
+    return moment(date).subtract(days, 'days').format('YYYY-MM-DD');
+  };
+
+  const { data: DataPix, isFetching } = useCustomers(
+    currentPage,
+    per_page,
+    'pix',
+    filterDate && formatPrevDate(+filterDate),
+    filterDate && moment(date).format('YYYY-MM-DD')
+  );
+
+  const { pagination } = createPagination(
+    5,
+    DataPix?.meta?.last_page || 10,
+    DataPix?.meta?.current_page || 0
+  );
+
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { data, isLoading } = useQuery(
     [2],
@@ -115,6 +142,10 @@ export default function Pix() {
     }
   }
 
+  // console.log(moment(moment('2022/10/24').add(7, 'days')).format('YYYY/MM/DD'));
+
+  // console.log(moment('2022/10/24').subtract(4, 'days').format('YYYY/MM/DD'));
+
   return (
     <Layout>
       <SimpleGrid
@@ -141,6 +172,40 @@ export default function Pix() {
         />
       </SimpleGrid>
       <Box bg="#FFFFFF" p="50px" mt="30px" borderRadius="10px">
+        <Text>Extrato: iHold Bank </Text>
+        <Flex my="10px" w="380px" justify="space-between">
+          <Button
+            transition="all linear .55s"
+            variant="unstyled"
+            fontSize="14px"
+            w="67px"
+            h="26"
+            borderRadius="52px"
+            border="1px solid #CBD3E0"
+            color={!filterDate ? '#fff' : ''}
+            bg={!filterDate ? '#2E4EFF' : ''}
+            onClick={() => setFilterDate('')}
+          >
+            Todos
+          </Button>
+          {dates.map((day, key) => (
+            <Button
+              key={key}
+              transition="all linear .55s"
+              variant="unstyled"
+              w="67px"
+              h="26"
+              fontSize="14px"
+              borderRadius="52px"
+              border="1px solid #CBD3E0"
+              color={+filterDate === day ? '#fff' : ''}
+              bg={+filterDate === day ? '#2E4EFF' : ''}
+              onClick={() => setFilterDate(day.toString())}
+            >
+              {day} dias
+            </Button>
+          ))}
+        </Flex>
         <Box w="full" borderRadius="6px" h="620px">
           <Flex mb="30px">
             <Icon
@@ -152,7 +217,7 @@ export default function Pix() {
             <Text ml="5px">EXTRATO PIX</Text>
           </Flex>
           <ContainerTransaction
-            tabName={['todos', 'entrada', 'Saída']}
+            tabName={['completo', 'entrada', 'Saída']}
             header={
               <Button
                 bg="#2E4EFF"
@@ -180,7 +245,7 @@ export default function Pix() {
             }
           >
             <TabPanel>
-              <ExtractPixAndTedTable isLoading={isLoading} items={data} />
+              <ExtractPixAndTedTable isLoading={isFetching} items={DataPix} />
             </TabPanel>
             <TabPanel>
               <ExtractPixAndTedTable
@@ -196,6 +261,64 @@ export default function Pix() {
             </TabPanel>
           </ContainerTransaction>
         </Box>
+        <Flex align="center" w="full" justify="center" mt="40px">
+          <Box cursor={!!DataPix?.links.prev ? 'pointer' : 'not-allowed'}>
+            <Icon
+              icon="dashicons:arrow-left-alt2"
+              color={!!DataPix?.links.prev ? '#7f8b9f' : '#ccc'}
+              width="20"
+              height="20"
+              onClick={() => {
+                if (DataPix?.links.prev) {
+                  setCurrentPage((page) => page - 1);
+                }
+              }}
+            />
+          </Box>
+
+          <Flex minW="250px" justify="center">
+            {!isFetching
+              ? pagination.map((item, key) => (
+                  <Button
+                    // mx="3px"
+                    borderRadius="6px"
+                    w="38px"
+                    h="38px"
+                    key={key}
+                    onClick={() => setCurrentPage(item)}
+                    color={currentPage === item ? '#FFFFFF' : '#CBD3E0'}
+                    bg={currentPage === item ? '#2E4EFF' : ''}
+                  >
+                    {item}
+                  </Button>
+                ))
+              : Array.from({ length: 5 }).map((_, key) => (
+                  <Skeleton
+                    w="38px"
+                    h="38px"
+                    key={key}
+                    borderRadius="6px"
+                    mx="3px"
+                  />
+                ))}
+          </Flex>
+          <Box cursor={!!DataPix?.links.next ? 'pointer' : 'not-allowed'}>
+            <Icon
+              icon="dashicons:arrow-right-alt2"
+              color={!!DataPix?.links.next ? '#7f8b9f' : '#ccc'}
+              width="20"
+              height="20"
+              style={{
+                cursor: DataPix?.links.next ? 'pointer' : 'not-allowed',
+              }}
+              onClick={() => {
+                if (DataPix?.links.next) {
+                  setCurrentPage((page) => page + 1);
+                }
+              }}
+            />
+          </Box>
+        </Flex>
       </Box>
       <Box bg="#FFFFFF" p="50px" mt="30px" borderRadius="10px">
         <Text>MAIS ACESSADOS</Text>
